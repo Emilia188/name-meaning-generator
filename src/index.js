1f03329680dbc178d032250448ba193913a2fee2
Emilia188 <emiliaadasiewicz@gmail.com>
2024-08-22 17:13:49 +0200

    Added basic form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateNameMeaning(event) {
+  event.preventDefault();
 
+  new Typewriter("#description", {
+    strings:
+      "The name Emilia is of Latin origin and is derived from the Roman family name Aemilius.",
+    autoStart: true,
+    cursor: "",
+    delay: 50,
+  });
+}
+
+let nameFormElement = document.querySelector("#name-generator-form");
+nameFormElement.addEventListener("submit", generateNameMeaning);
